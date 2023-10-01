48f058834230dca4da6113735f86ec6741b208db
2166316 <antou98@live.com>
2023-10-01 16:44:37 -0400

    ajout

--- appJsBacketFront/client/src/App.js
+++ appJsBacketFront/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component,useEffect,useState } from 'react'
 import PropTypes from 'prop-types';
 
+
 //In React, the useEffect hook is designed to be used in functional components
 function FetchApi1EtAffiche() {
 
@@ -17,35 +18,33 @@ function FetchApi1EtAffiche() {
   )
 }
 
-function FetchApi1Data(props){
-  const [backEndData,setbackEndData] = useState({});
-  useEffect(()=>{fetch("/api").then(response => response.json()).then(data =>{setbackEndData(data.users)})},[])
-  console.log(backEndData);
-      
-  let isUser = false
-  let message ="not connected"
-  console.log(backEndData.length)
-  for(let i =0;i<backEndData.length;i++){
-    if(backEndData[i].username===props.username && backEndData[i].password===props.password){
-      isUser = true;
-    }  
-    console.log("vals   "+backEndData[i].username +"  "+backEndData[i].password)    
-  }
+function FetchApi1Data(nameUser,passWord){
+  let userData = {"username": nameUser,"password": passWord}
 
-  console.log("props : "+props.username + "  "+props.password)
-  if(isUser){
-    
-      message= "is connected"
- 
-  }
-  else{
-  
-    message= "not connected"
+  let estUnUser = false; 
 
-  }
-  
-    console.log(message)
-    return <>{message}</>;
+  const responseData = fetch("/api",{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData)
+    }).then(response =>{
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Network response was not ok.');
+      }
+    }).then(data=>{
+      console.log(data.estUtilisateur)
+      return data.estUtilisateur
+    })
+    .catch(error =>{
+      console.log("error call api")
+    })
+
+    estUnUser = responseData
+    return estUnUser;
 }
 
 function FetchApi2() {
@@ -71,7 +70,7 @@ export class App extends Component {
     this.state = {
         userName : '',
         passWord : '',
-        connect : 'not connected'
+        connect : ''
 
     }
     this.handleInputChange = this.handleInputChange.bind(this)
@@ -91,8 +90,14 @@ export class App extends Component {
     });
   }
 
-  tryConnect(){
-      
+  async tryConnect() {
+    const result = await FetchApi1Data(this.state.userName, this.state.passWord);
+    if(result){
+      this.setState({ connect: "connection" });
+    }
+    else{
+      this.setState({connect: "pas connecté"})
+    }
   }
 
   
@@ -105,7 +110,7 @@ export class App extends Component {
         <input type='text' name='userName' onChange={this.handleInputChange} value={this.state.userName}></input>
         <input type='text' name='passWord' onChange={this.handleInputChange} value={this.state.passWord}></input>
         <button onClick={this.tryConnect}>Click</button>
-        <FetchApi1Data username={this.state.userName} password={this.state.passWord}/>
+        <p>status : {this.state.connect}</p>
       </div>
     
     )
--- appJsBacketFront/server/server.js
+++ appJsBacketFront/server/server.js
@@ -1,12 +1,41 @@
 const express = require('express')
-
+const bodyParser = require('body-parser');
 
 const userArray = { "users":[{"username":'DickClub',"password":"123"},{"username":'gino',"password":"abc"},{"username":'gio',"password":"321"}],"admins":[{"username":"dickbitch","password":"cock"}]}
 const app = express()
-// page /api
-app.get("/api",(req,res)=>{
-    res.json(userArray)
-});
+//pour passer du json à partir du client
+ app.use(bodyParser.json());
+
+// page /api pour passer data
+//app.get("/api",(req,res)=>{
+//    res.json(userArray)
+//});
+
+app.use(bodyParser.json());
+
+//pour requete
+app.post("/api",(req,res)=>{
+    const {username,password} = req.body;
+    console.log(username)
+    console.log(password)
+    let isAUser = false;
+    //pour users
+    userArray.users.forEach(element => {
+        if(username === element.username && password === element.password){
+            isAUser = true;
+           
+        }
+    });
+
+    //pour admins
+    userArray.admins.forEach(element => {
+        if(username === element.username && password === element.password){
+            isAUser = true;
+        }
+    });
+
+    res.json({"estUtilisateur":isAUser})
+})
 
 app.get("/api2",(req,res)=>{
     res.end("Hello")
@@ -19,3 +48,5 @@ app.get("/api3",(req,res)=>{
 app.listen(5000,()=>{
     console.log("server listening on port 5000")
 });
+
+//

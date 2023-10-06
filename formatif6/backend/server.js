7354edbfcd40ada7e5d6b11d9521d0c1f16a76bf
Antoine Fortier <2166316@cegeplimoilou.ca>
2023-10-06 13:34:24 -0400

    ajout au formatif 6 voir body req

--- formatif6/backend/server.js
+++ formatif6/backend/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 
+//nécessaire
+app.use(express.json());
+
 //res.send() is used to send the response to the client where res.end() is used to end the response you are sending.
 app.get("/apiGet1",(req,res)=>{
     res.send("allo").end();
@@ -13,19 +16,38 @@ app.get("/apiGet2",(req,res)=>{
     //res.end("Hello")
 });
 
+// ajouter pour voir body json app.use(express.json());
 app.post("/apiPost",(req,res)=>{
+    //const {username,password} = req.body;
+    console.log(req.body)
+    //console.log(username)
+    //console.log(password)
     res.json({"val":"hello world"}).end();
 })
 
-app.listen(5000,()=>{
-    console.log("server listening on port 5000")
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log("server listening on port "+port)
 });
 
+
+
+
 //tous les requetes passe par ici
-app.use((req,res)=>{
+/*app.use((req,res,next)=>{
     console.log("à chaque appel use appelé");
 
     //appel la prochaine app.use
-    next()
+    next();
 
 })
+
+app.use((req,res,next)=>{
+    console.log("à chaque appel use appelé aussi par la première");
+    end();
+    //appel la prochaine app.use
+    
+
+})*/
